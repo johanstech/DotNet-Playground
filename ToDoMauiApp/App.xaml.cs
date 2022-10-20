7d49d66716bea081671dd6f4c30c0d3b798bebd7
Johan Andersson <johanstechs@gmail.com>
2022-10-20 03:00:59 +0200

    Added new Maui App client for ToDo API

---
+++ ToDoMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ToDoMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
